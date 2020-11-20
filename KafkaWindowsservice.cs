6960904ee493afaf22a3b35400a222beb80af3e3
knowledge0603 <23024084@qq.com>
2020-11-20 22:13:23 +0800

    remark

---
+++ KafkaWindowsservice.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.ServiceProcess;
+using System.Text;
+
+namespace KafkaService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            string volume = AppDomain.CurrentDomain.BaseDirectory.Substring(0, AppDomain.CurrentDomain.BaseDirectory.IndexOf(":"));
+            System.IO.Directory.SetCurrentDirectory(volume + ":\\kafka\\bin\\windows\\");
+
+            wLog("kafka Service Start");
+            startKafka();
+        }
+
+        protected override void OnStop()
+        {
+            // TODO: 在此处添加代码以执行停止服务所需的关闭操作。
+            //CloseKafka();
+            KillProcessAndChildren(kafkaProcess.Id);
+            wLog("kafka  Service Stop\n");
+        }
+
+        private Process kafkaProcess=null;
+        private void startKafka(Object sender = null, EventArgs e = null)
+        {
+            kafkaProcess = StartProcess(Constants.KafkaProcess);
+        }
+
+        private static Process StartProcess(string command)
+        {
+            var processInfo = new ProcessStartInfo("cmd.exe", "/c " + command)
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                Verb = "runas"
+            };
+
+            return Process.Start(processInfo);
+        }
+
+        private static void KillProcessAndChildren(int pid)
+        {
+            using (var searcher = new ManagementObjectSearcher("Select * From Win32_Process Where ParentProcessID=" + pid))
+            {
+                var managementObjects = searcher.Get();
+
+                foreach (var obj in managementObjects)
+                {
+                    var managementObject = (ManagementObject)obj;
+                    KillProcessAndChildren(Convert.ToInt32(managementObject["ProcessID"]));
+                }
+
+                try
+                {
+                    var proc = Process.GetProcessById(pid);
+                    proc.Kill();
+                }
+                catch (ArgumentException)
+                {
+                    // Process already exited.
+                    wLog("ArgumentException",true);
+                }
+            }
+        }
+        private static void wLog(string logStr, bool wTime = true)
+        {
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\KafkaAutoService.log", true))
+            {
+                string timeStr = wTime == true ? DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss ") : "";
+                sw.WriteLine(timeStr + logStr);
+            }
+        }
+
+    }
+}
